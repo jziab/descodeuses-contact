3e7c22717ea878d13e8ee0eedce9d179583345cf
jziab <ziabjega98@gmail.com>
2025-06-26 16:02:41 +0200

    des codeuses contact app

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatCardModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
